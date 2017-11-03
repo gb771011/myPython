8c674f7e5f5c82e0cb023eac1e6f28d84feb00f4
gb771011 <gb771011@gmail.com>
2017-11-03 12:22:49 +0800

    123

---
+++ operator.py
@@ -0,0 +1,28 @@
+print('Operator:')
+# 基本運算子: + - * / 
+a=7
+b=3
+list=[1,2,3,4,5];
+
+print('a+b =',a+b)
+print('a-b =',a-b)
+print('a*b =',a*b)
+print('a/b =',a/b)
+
+#基本運算子: %(傳回餘數) **(次方) //(傳回商數)
+
+print('a%b = ',a%b)
+print('a**b = ',a**b)
+print('a//b = ',a//b)
+print('----------')
+
+print('a>b : ',a>b)
+print('a>=b : ',a>=b)
+print('a<b : ',a<b)
+print('a<=b : ',a<=b)
+print('a==b : ',a==b)
+print('a!=b : ',a==b)
+
+print('----------')
+print('a in list : ',list,a in list)
+print('b in list : ',list,b in list)
