78842faf256144c93fee1d8d38a597a70f4c9ae3
gb771011 <gb771011@gmail.com>
2017-11-06 17:21:25 +0800

    update

---
+++ loop-for.py
@@ -0,0 +1,4 @@
+list=[1,9,2,8,'apple',True]
+for i in list:
+    print(i)
+    pass
---
+++ loop-if.py
@@ -0,0 +1,11 @@
+x=4
+y=4
+z=3
+
+if x>y:
+    print("x>y")
+elif x==y:
+    print('x=y')
+    pass
+else:
+    print("x<y")
